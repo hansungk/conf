56adae4483c18a4559201ab45adc2ab23f80730e
Hansung Kim <stephen422@gmail.com>
2020-04-19 16:21:37 +0900

    build-llvm.sh: manual mode

--- build-llvm.sh
+++ build-llvm.sh
@@ -15,37 +15,38 @@
 # Some useful docs.
 # Purpose of LLVM_ENABLE_LIBCXX: http://lists.llvm.org/pipermail/llvm-dev/2015-July/088689.html
 
-if [ "$#" -ge 1 ]; then
-    REV=${1}
-else
-    REV=master
-fi
-
 export CC=${CC:-/usr/bin/clang}
 export CXX=${CXX:-/usr/bin/clang++}
-timestamp=$(date +'%y%m%d-%H%M%S')
-prefix=${HOME}/build/llvm-${REV}-${timestamp}
-workdir=${PWD}/build-llvm-${REV}-${timestamp}
-srcdir=${workdir}/llvm-project
-builddir=${workdir}/build
 
-mkdir ${workdir}
-cd ${workdir}
-
-echo ">>> Fetching LLVM."
-if [[ ! -d ${srcdir} ]]; then
-    echo "REV: ${REV}"
-    git clone https://github.com/llvm/llvm-project ${srcdir}
+if [ "$#" -ge 1 ]; then
+    srcdir=${PWD}/${1}
 else
-    echo "Skipping clone."
+    echo "usage: ${0} srcdir name"
+    exit 1
 fi
-pushd ${srcdir}
-git checkout ${REV}
-# git pull --depth 1 origin ${REV}
-# git gc --prune=all
-popd
+if [ "$#" -ge 2 ]; then
+    tag=${2}
+else
+    timestamp=$(date +'%y%m%d-%H%M%S')
+    tag=${timestamp}
+fi
+prefix=${HOME}/build/llvm-${tag}
+builddir=${PWD}/build-llvm-${tag}
 
-echo ""
+# echo ">>> Fetching LLVM."
+# if [[ ! -d ${srcdir} ]]; then
+#     echo "REV: ${REV}"
+#     git clone --depth 100 https://github.com/llvm/llvm-project ${srcdir}
+# else
+#     echo "Skipping clone."
+# fi
+# if [ ${REV} != "master" ]; then
+#     pushd ${srcdir}
+#     git checkout ${REV}
+#     popd
+# fi
+# echo ""
+# 
 echo ">>> Configuring LLVM."
 mkdir ${builddir}
 cd ${builddir}
@@ -81,11 +82,13 @@ cmake_args=(
 # -DSANITIZER_USE_STATIC_LLVM_UNWINDER=On
 
 if [ "$(uname)" == "Darwin" ]; then
+    cmake_args+=( -DDEFAULT_SYSROOT=$(xcrun --sdk macosx --show-sdk-path))
+    cmake_args+=( -DLLVM_TARGETS_TO_BUILD=X86)
     cmake_args+=( -DLLVM_CREATE_XCODE_TOOLCHAIN=ON)
     cmake_args+=( -DCOMPILER_RT_ENABLE_IOS=Off)
     cmake_args+=( -DLLVM_BUILD_EXTERNAL_COMPILER_RT=ON)
     cmake_args+=( -DLLVM_ENABLE_LIBCXX=ON)
-    cmake_args+=( -DDEFAULT_SYSROOT=$(xcrun --sdk macosx --show-sdk-path))
+    cmake_args+=( -DLLDB_USE_SYSTEM_DEBUGSERVER=ON)
 else
     # These args set the Clang's default behavior when building other programs,
     # tend to wreck LLVM tests because there's usually no existing libc++ in
@@ -109,8 +112,8 @@ else
     # cmake_args+=( -DLLVM_LIBDIR_SUFFIX=64)
 fi
 
-echo ">>> Configured arguments:"
-echo ${cmake_args[*]}
+# echo ">>> Configured arguments:"
+# echo ${cmake_args[*]}
 
 cmake ${srcdir}/llvm -G Ninja ${cmake_args[@]}
 
@@ -120,7 +123,23 @@ cmake --build . -- -v
 
 echo ""
 echo ">>> Checking."
-# check-lldb fails
+# Tests failing on Darwin:
+# - check-tsan
+# - check-lldb-api
+# - check-clang:
+#      Clang :: Driver/darwin-header-search-libcxx.cpp
+#      Clang :: Driver/darwin-header-search-system.cpp
+#      Clang :: Driver/hexagon-toolchain-elf.c
+#      Clang :: Driver/mingw-sysroot.cpp
+#      Clang :: Driver/mips-cs.cpp
+#      Clang :: Driver/mips-fsf.cpp
+#      Clang :: Driver/mips-img-v2.cpp
+#      Clang :: Driver/mips-img.cpp
+#      Clang :: Driver/ps4-header-search.c
+#      Clang :: Driver/riscv32-toolchain-extra.c
+#      Clang :: Driver/riscv64-toolchain-extra.c
+#      Clang :: Driver/sysroot.c
+#      Clang :: Frontend/warning-poison-system-directories.c
 cmake --build . --target check-llvm check-clang check-cxx check-cxxabi check-lld
 
 echo ""
