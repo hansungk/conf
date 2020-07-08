ec54698ebcad5a4f2b5d6151bc64619308681aeb
Hansung Kim <stephen422@gmail.com>
2020-07-08 20:45:32 +0900

    Path for doom

--- zshrc.d/emacs.zsh
+++ zshrc.d/emacs.zsh
@@ -2,3 +2,5 @@ export PATH="$HOME/build/emacs-27.0.91/bin:$PATH"
 if [[ "$(uname)" == "Darwin" ]]; then
     export PATH="$HOME/src/emacs/nextstep/Emacs.app/Contents/MacOS:$PATH"
 fi
+
+export PATH="$HOME/.emacs.d/bin:$PATH"
