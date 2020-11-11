000a91bacb2f4b302ba5e25ecdcf748dfef80b14
Hansung Kim <stephen422@gmail.com>
2020-11-11 17:54:41 +0900

    xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,8 +2,9 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
 -- Imports --
--- stuff
 import XMonad
+
+import XMonad.Actions.CycleWS
 
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageHelpers
@@ -12,36 +13,74 @@ import XMonad.Hooks.UrgencyHook
 import XMonad.Hooks.InsertPosition
 import XMonad.Hooks.EwmhDesktops
 
-import XMonad.Layout.NoBorders
-import XMonad.Layout.ResizableTile
+import XMonad.Layout.NoBorders as NB
+import XMonad.Layout.Spacing
+-- import XMonad.Layout.ResizableTile
 import XMonad.Layout.Renamed
+import XMonad.Layout.Fullscreen
 import XMonad.Layout.Tabbed
 
 import XMonad.Util.Run (spawnPipe, hPutStrLn)
 
--- stock
 import qualified Data.Map as M
+import Control.Monad (when, join)
+import Data.Maybe (maybeToList)
 
+-- Notes.
+--
+-- Fullscreen:
+-- - see https://github.com/xmonad/xmonad-contrib/issues/183#issuecomment-307407822
+--
 ------------------------------------------------------------------------------------------
 -- Main Configuration
 main :: IO()
 --main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
 main = do
-	h <- spawnPipe myBar
-	xmonad $ ewmh defaultConfig	{ manageHook = manageDocks <+> manageHook defaultConfig
-								, layoutHook = vanillaLayoutHook
-								, logHook = vanillaLogHook h
-								, modMask = mod1Mask
-								, terminal = "urxvt"
-								, borderWidth = 1
-	--							, focusedBorderColor = (colLook Red 0)
-	--							, normalBorderColor = (colLook Black 0)
-								}
+        h <- spawnPipe myBar
+        xmonad $ ewmh $ fullscreenSupport $ docks $ myConfig h
+
+myConfig p = def
+        { manageHook = myManageHook <+> manageHook defaultConfig -- merge with default
+        , handleEventHook = XMonad.Layout.Fullscreen.fullscreenEventHook -- FIXME
+        , layoutHook = myLayoutHook
+        , logHook = myLogHook p
+        , startupHook = addEWMHFullscreen
+        , modMask = mod4Mask
+        , terminal = "st"
+        , keys = \c -> mykeys c `M.union` keys def c
+        , borderWidth = 3
+        , focusedBorderColor = (colLook Red 0)
+        , normalBorderColor = (colLook Black 0)
+        }
 
 
 ------------------------------------------------------------------------------------------
+-- Keybindings
+mykeys (XConfig {modMask = modm}) = M.fromList $
+        [ ((modm, xK_Tab),    toggleWS)
+        , ((modm , xK_p),     spawn "rofi -modi run,drun -show run")
+        , ((modm, xK_b     ), sendMessage ToggleStruts)]
+
+------------------------------------------------------------------------------------------
 -- Constants
-myBar = "/usr/bin/xmobar /home/stephen/.xmobarrc"
+myBar = "xmobar /home/hansung/.xmobarrc"
+
+
+------------------------------------------------------------------------------------------
+-- Manage hook
+myManageHook :: ManageHook
+myManageHook =
+        manageDocks
+    <+> XMonad.Layout.Fullscreen.fullscreenManageHook
+    <+> composeAll
+            [ className =? "kakaotalk.exe" --> doFloat
+            ]
+
+
+------------------------------------------------------------------------------------------
+-- Handle event hook
+myHandleEventHook = handleEventHook def
+                <+> XMonad.Layout.Fullscreen.fullscreenEventHook
 
 
 ------------------------------------------------------------------------------------------
@@ -49,31 +88,30 @@ myBar = "/usr/bin/xmobar /home/stephen/.xmobarrc"
 -- configure xmobar looks
 
 -- Pretty with colored background
-myLogHook h = dynamicLogWithPP myPP
-	where
-		myPP = defaultPP	{ ppCurrent	= xmobarColor (colLook White 1) (colLook Green 0) . wrap " " " "
-							, ppVisible	= xmobarColor (colLook Green 1) "" . wrap " " " "
-							, ppHidden	= xmobarColor (colLook White 1) "" . wrap " " " "
-							, ppHiddenNoWindows = xmobarColor (colLook Black 0) "" . wrap " " " "
-							, ppUrgent	= xmobarColor (colLook White 1) "" . wrap "[" "]"
-							, ppLayout	= xmobarColor (colLook Green 1) "" .
-								(\x -> case x of
-									"Spacing 7 Tall"	-> "[]="
-									"Tall"				-> "[]="
-									"Full"				-> "[F]"
-									_					-> x
-								)
-							, ppTitle	= xmobarColor (colLook White 1) "" . shorten 80
-							, ppSep		= xmobarColor (colLook Grey 0) "" " | "
-							, ppWsSep	= xmobarColor "" "" "" -- Eliminates the gap
-							, ppOutput	= hPutStrLn h
-							}
+myLogHook h = dynamicLogWithPP $ def
+        { ppCurrent	= xmobarColor (colLook White 1) (colLook Green 0) . wrap " " " "
+        , ppVisible	= xmobarColor (colLook Green 1) "" . wrap " " " "
+        , ppHidden	= xmobarColor (colLook White 1) "" . wrap " " " "
+        , ppHiddenNoWindows = xmobarColor (colLook Black 0) "" . wrap " " " "
+        , ppUrgent	= xmobarColor (colLook White 1) "" . wrap "[" "]"
+        , ppLayout	= xmobarColor (colLook Green 1) "" .
+        (\x -> case x of
+         "Spacing 7 Tall" -> "[]="
+         "Tall"           -> "[]="
+         "Full"	          -> "[F]"
+         _                -> x
+        )
+        , ppTitle	= xmobarColor (colLook White 1) "" . shorten 80
+        , ppSep		= xmobarColor (colLook Grey 0) "" " | "
+        , ppWsSep	= xmobarColor "" "" "" -- Eliminates the gap
+        , ppOutput	= hPutStrLn h
+        }
 
 -- Vanilla config
-vanillaLogHook h = dynamicLogWithPP xmobarPP
-					{ ppOutput = hPutStrLn h
-					, ppTitle = xmobarColor "green" "" . shorten 80
-					}
+-- myLogHook h = dynamicLogWithPP xmobarPP
+--         { ppOutput = hPutStrLn h
+--         , ppTitle = xmobarColor "green" "" . shorten 80
+--         }
 
 
 ------------------------------------------------------------------------------------------
@@ -81,7 +119,7 @@ vanillaLogHook h = dynamicLogWithPP xmobarPP
 -- configure layouts
 
 -- Useless gaps
---gaplessLayoutHook = avoidStruts $
+-- gaplessLayoutHook = avoidStruts $
 --	tiled ||| Full ||| fullTiled
 --	where 
 --		tiled		= spacing 7 $ Tall nmaster delta ratio
@@ -94,12 +132,16 @@ vanillaLogHook h = dynamicLogWithPP xmobarPP
 --		-- Default proportion of screen occupied by master pane
 --		ratio	= 1/2
 
--- Vanilla
-vanillaLayoutHook = avoidStruts  $  layoutHook defaultConfig
+myLayoutHook =
+        NB.lessBorders NB.OnlyScreenFloat $
+        avoidStruts $
+        spacingRaw True (Border 0 10 10 10) True (Border 10 10 10 10) True $
+        layoutHook defaultConfig
 
 ------------------------------------------------------------------------------------------
 -- Key Bindings
 -- Bar
+-- FIXME
 toggleStrutsKey :: XConfig Layout -> (KeyMask, KeySym)
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
@@ -144,3 +186,24 @@ colors = M.fromList
 	, (BG	 	, (	"#000000",
 					"#444444"))
 	]
+
+
+------------------------------------------------------------------------------------------
+-- Fullscreen fix
+--
+-- See: https://github.com/xmonad/xmonad-contrib/issues/183#issuecomment-307407822
+addNETSupported :: Atom -> X ()
+addNETSupported x   = withDisplay $ \dpy -> do
+    r               <- asks theRoot
+    a_NET_SUPPORTED <- getAtom "_NET_SUPPORTED"
+    a               <- getAtom "ATOM"
+    liftIO $ do
+       sup <- (join . maybeToList) <$> getWindowProperty32 dpy a_NET_SUPPORTED r
+       when (fromIntegral x `notElem` sup) $
+         changeProperty32 dpy r a_NET_SUPPORTED a propModeAppend [fromIntegral x]
+
+addEWMHFullscreen :: X ()
+addEWMHFullscreen   = do
+    wms <- getAtom "_NET_WM_STATE"
+    wfs <- getAtom "_NET_WM_STATE_FULLSCREEN"
+    mapM_ addNETSupported [wms, wfs]
