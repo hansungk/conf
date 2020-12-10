3aee3d4447ad55d70883ced250462a40ef8f315d
Hansung Kim <hansung_kim@berkeley.edu>
2020-12-09 22:33:04 -0800

    emacs: a little saner Verilog mode

--- emacs-prelude/lang.el
+++ emacs-prelude/lang.el
@@ -6,4 +6,18 @@
   (global-hl-line-mode -1))
 (add-hook 'prog-mode-hook 'my-prog-mode-hook)
 
-(setq verilog-auto-newline nil)
+;; Verilog
+(defun my-verilog-mode-hook ()
+  (setq indent-tabs-mode nil)
+  (setq tab-width 2)
+  (setq verilog-indent-level 2
+        verilog-indent-level-behavioral 2
+        verilog-indent-level-module 2
+        verilog-indent-level-declaration 2
+        verilog-auto-newline nil
+        verilog-tab-always-indent t)
+  (set-fill-column 80)
+  ;; (define-key verilog-mode-map (kbd "RET") 'electric-indent-just-newline)
+  ;; (define-key verilog-mode-map (kbd "TAB") 'tab-to-tab-stop)
+  )
+(add-hook 'verilog-mode-hook 'my-verilog-mode-hook)
