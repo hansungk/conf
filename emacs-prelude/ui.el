d16e18fa0be5f0996787471b7843e95b316d0ad4
Hansung Kim <stephen422@gmail.com>
2020-12-13 20:46:05 +0900

    emacs: fix undefined prelude-mode-map error

--- emacs-prelude/ui.el
+++ emacs-prelude/ui.el
@@ -15,8 +15,11 @@
 (global-hl-line-mode -1)
 (setq global-hl-line-mode -1)
 
+(add-hook 'prelude-mode-hook
+	  (lambda ()
+	    (define-key prelude-mode-map (kbd "C-a") 'move-beginning-of-line)))
+
 (global-set-key (kbd "C-a") 'move-beginning-of-line)
-(define-key prelude-mode-map (kbd "C-a") 'move-beginning-of-line)
 (global-set-key (kbd "C-c j") 'counsel-rg)
 (global-set-key (kbd "C-c '") 'avy-goto-char-2)
 (global-set-key (kbd "C-c ;") 'avy-goto-char-in-line)
